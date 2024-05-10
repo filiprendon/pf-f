82b58082f1a532988edf514da9546bc2c16b2a35
filiprendon <frendont2223@politecnics.barcelona>
2024-05-10 20:14:08 +0200

    .

---
+++ script.js
@@ -0,0 +1,12 @@
+function typeWriter(element, text, i = 0) {
+    if (i < text.length) {
+      element.textContent += text.charAt(i);
+      setTimeout(() => typeWriter(element, text, i + 1), typingSpeed); // Adjust typingSpeed for desired speed
+    }
+  }
+  
+  const element = document.querySelector(".typing-text");
+  const text = "Filip Rendón - Web Developer";
+  const typingSpeed = 50; // Adjust this value to change the typing speed (in milliseconds)
+  
+  typeWriter(element, text);
