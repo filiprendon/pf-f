7130f4df546c037ef70333b2f81d29f0c568d281
filiprendon <126860088+filiprendon@users.noreply.github.com>
2024-05-12 13:49:17 +0200

    .

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function typeWriter(element, text, i = 0) {
     if (i < text.length) {
       element.textContent += text.charAt(i);
-      setTimeout(() => typeWriter(element, text, i + 1), typingSpeed); // Adjust typingSpeed for desired speed
+      setTimeout(() => typeWriter(element, text, i + 1), typingSpeed);
     }
   }
   
   const element = document.querySelector(".typing-text");
   const text = "Filip Rendón - Web Developer";
-  const typingSpeed = 50; // Adjust this value to change the typing speed (in milliseconds)
+  const typingSpeed = 40;
   
   typeWriter(element, text);
